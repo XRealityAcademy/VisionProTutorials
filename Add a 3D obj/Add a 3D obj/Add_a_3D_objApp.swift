7681693cdc73939840b8f1efc5012fd9f64c2f10
XReality Academy <88015426+XRealityAcademy@users.noreply.github.com>
2023-08-25 16:18:39 -0700

    add a 3D Obj

---
+++ Add a 3D obj/Add a 3D obj/Add_a_3D_objApp.swift
@@ -0,0 +1,21 @@
+//
+//  Add_a_3D_objApp.swift
+//  Add a 3D obj
+//
+//  Created by Kuan Yi Wu on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Add_a_3D_objApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }.immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
